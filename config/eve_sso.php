6734f13495a98461c4b6942c512c11ac753940d8
Andre 'Necrotex' Peiffer <necrotex@gmail.com>
2016-02-13 13:50:04 +0100

    updated meta and config files

---
+++ config/eve_sso.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+	'url_auth' => 'https://login.eveonline.com/oauth/authorize',
+	'url_verify' => 'https://login.eveonline.com/oauth/verify',
+	'url_token' => 'https://login.eveonline.com/oauth/token',
+	'client_id' => env('EVE_SSO_CLIENT_ID'),
+	'client_secret' => env('QbzObahNL5Y2L0YFHHmbowZ0598lZNAUjRB5xkGm')
+];
